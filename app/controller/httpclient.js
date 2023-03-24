ed8cad04bb4f7fc7a2cce597f7eb52a77c466d00
liugaowei <liugaowei.sd@chinatelecom.cn>
2023-03-24 23:31:59 +0800

    fetch换成ctx.curl

--- app/controller/httpclient.js
+++ app/controller/httpclient.js
@@ -4,11 +4,15 @@ const fs = require("fs");
 const Controller = require("egg").Controller;
 
 class HttpclientController extends Controller {
+  async test() {
+    const ctx = this.ctx;
+    ctx.body = ctx.request.body;
+  }
   async conversation() {
     const ctx = this.ctx;
     let res = null;
     try {
-      res = await fetch("https://aicursor.com/conversation", {
+      res = await ctx.curl("https://aicursor.com/conversation", {
         method: "post",
         headers: {
           "Sec-Fetch-Mode": "cors",
@@ -19,17 +23,14 @@ class HttpclientController extends Controller {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Cursor/0.1.1 Chrome/108.0.5359.62 Electron/22.0.0 Safari/537.36",
         },
-        body: JSON.stringify(ctx.request.body),
-      }).then((res) => res.clone().text());
+        referrerPolicy: "strict-origin-when-cross-origin",
+        contentType: "application/json",
+        data: JSON.stringify(ctx.request.body),
+      });
     } catch (error) {
       res = error;
     }
-
-    ctx.body = res;
-  }
-  async test() {
-    const ctx = this.ctx;
-    ctx.body = ctx.request.body;
+    ctx.body = res.data.toString();
   }
 }
 
